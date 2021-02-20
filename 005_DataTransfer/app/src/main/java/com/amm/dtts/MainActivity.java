cc4aac6ccf7d2aac4da2d4aab9c175dbacccdb90
Aung Myat Moe <aungmyatmoe834@gmail.com>
2021-02-20 17:16:45 +0630

    Add Raw File

--- 005_DataTransfer/app/src/main/java/com/amm/dtts/MainActivity.java
+++ 005_DataTransfer/app/src/main/java/com/amm/dtts/MainActivity.java
@@ -4,12 +4,17 @@ import android.app.*;
 import android.os.*;
 import android.widget.*;
 import java.security.*;
+import android.view.View.*;
+import android.view.*;
+import android.media.*;
 
 public class MainActivity extends Activity 
+implements OnClickListener
 {
 	private TextView mainTv,tvResult;
 	private EditText  mainEt;
 	private Button startAct,secStartAct;
+	private MediaPlayer mp;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -21,5 +26,27 @@ public class MainActivity extends Activity
 		mainEt = findViewById(R.id.mainEt);
 		startAct = findViewById(R.id.startAct);
 		secStartAct = findViewById(R.id.secStartAct);
+		
+		mainTv.setOnClickListener(this);
+		startAct.setOnClickListener(this);
+		secStartAct.setOnClickListener(this);
+		
+		mp = MediaPlayer.create(MainActivity.this,R.raw.array);
     }
+	
+	@Override
+	public void onClick(View v)
+	{
+		switch(v.getId()){
+			// play music when title is clicked
+			case R.id.mainTv:
+				mp.start();
+				break;
+			case R.id.startAct:
+				break;
+			case R.id.secStartAct:
+				break;
+		}
+	}
+	
 }

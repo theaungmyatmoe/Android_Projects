cf4986f3169a268436bf643e51d1e15db8f9de75
Aung Myat Moe <aungmyatmoe834@gmail.com>
2021-03-28 23:18:29 +0630

    Created First Circle

---
+++ 011_Realtive_Layout/app/src/main/java/com/amm834/relative/MainActivity.java
@@ -0,0 +1,14 @@
+package com.amm834.relative;
+
+import android.app.*;
+import android.os.*;
+
+public class MainActivity extends Activity 
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
